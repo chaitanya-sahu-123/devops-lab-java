1abe6521e2f84f75ee2865dd9f7d8d9ad84004cd
Chaitanya Sahu <chaitanya.sahu666@gmail.com>
2024-10-17 20:32:06 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello there, from Java!");
+    System.out.println("I am Chaitanya Sahu!!!");
+    System.out.println("Nice to meet you");
+  }
+}
